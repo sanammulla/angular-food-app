bd8463442213e7ea5871907dbc272c82845a3177
sipandey <siddharth.pandey06@gmail.com>
2018-09-05 22:51:06 +0530

    observables and cleanup

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,3 @@
-import { Recipe } from './recipe.model';
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from './recipe.service';
 
@@ -10,16 +9,9 @@ import { RecipeService } from './recipe.service';
 })
 export class RecipesComponent implements OnInit {
 
-  selectedRecipe: Recipe;
-
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    );
   }
 
 }
